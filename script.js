10584f65c873689137b7546a80a689435a2d41be
lopez51 <rodriguez.jose@escola.pr.gov.br>
2025-09-09 12:04:54 -0300

    Initial commit

---
+++ script.js
@@ -0,0 +1,4 @@
+document.getElementById('login-btn').addEventListener('click', function() {
+    alert('O botão de login foi clicado!');
+    // Aqui você pode adicionar a lógica para abrir um formulário de login
+});
